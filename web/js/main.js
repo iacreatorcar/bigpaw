e2f5dfb16591c4a4582d8bf814344bce51ff0823
iacreatorcar <aerialoperator@outlook.com>
2026-02-22 23:25:09 +0100

    Connesso backend live su Render

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,5 @@
 // file: web/js/main.js
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://bigpaw-backend.onrender.com/api';
 
 // Carica le statistiche
 async function caricaStatistiche() {
